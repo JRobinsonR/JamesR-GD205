9c26d1f387fbc6ab2524e9a944a8482a87286794
Steven <stevenadrian02@hotmail.com>
2019-09-18 16:41:46 -0400

    Dark World Travel (James R)

--- GD205-28dc38dee9fb368c11a338dff964dc496ecc9dd3/James' Unity Projects/Assets/Scripts/PlayerMovement.cs
+++ GD205-28dc38dee9fb368c11a338dff964dc496ecc9dd3/James' Unity Projects/Assets/Scripts/PlayerMovement.cs
@@ -7,9 +7,11 @@ public class PlayerMovement : MonoBehaviour
 
 public Vector3 pos = new Vector3(5f,10f,15f);
 public Transform playerPiece;
-public Transform warpR;
-public Transform warpW;
+public Transform warpR, warpW;
+public Transform obstacle;
+public Transform RoadBlocks;
 public Vector3 playerStart;
+public TextMesh playerMsg;
 
     // Start is called before the first frame update
     void Start()
@@ -22,44 +24,51 @@ public Vector3 playerStart;
     {
         if (Input.GetKeyDown(KeyCode.W)) 
         {
-           
+           if(obstacle.position != playerPiece.position +new Vector3(0f,0f, 2.5f)){
             //modify the playerPiece position by tileAmount on the z axis
-            playerPiece.position += new Vector3(0f,0f, 5f);
+            playerPiece.position += new Vector3(0f,0f, .5f);
+            }
     }
     if (Input.GetKeyDown(KeyCode.S)) 
         {
-            
+            if(obstacle.position != playerPiece.position +new Vector3(0f,0f, -2.5f)){
             //modify the playerPiece position by tileAmount on the z axis
-            playerPiece.position += new Vector3(0f,0f, -5f);
+            playerPiece.position += new Vector3(0f,0f, -.5f);
+        }
         }
     if (Input.GetKeyDown(KeyCode.UpArrow)) 
         {
-            
+            if(obstacle.position != playerPiece.position +new Vector3(0f,2.5f, 0f)){
             //modify the playerPiece position by tileAmount on the z axis
-            playerPiece.position += new Vector3(0f,5f, 0f);
+            playerPiece.position += new Vector3(0f,.5f, 0f);
             }
-             if (Input.GetKeyDown(KeyCode.DownArrow)) {
-           
+            }
+     if (Input.GetKeyDown(KeyCode.DownArrow)) {
 
+        if(obstacle.position != playerPiece.position +new Vector3(0f,-2.5f, 0f)){
             //modify the playerPiece position by tileAmount on the z axis
-            playerPiece.position += new Vector3(0f,-5f, 0f);
+            playerPiece.position += new Vector3(0f,-.5f, 0f);
         }
-         
-         if (Input.GetKeyDown(KeyCode.LeftArrow)) 
+         }
+     if (Input.GetKeyDown(KeyCode.LeftArrow)) 
         {
-            
-
+        if(obstacle.position != playerPiece.position +new Vector3(2.5f,0f, 0f)){
             //modify the playerPiece position by tileAmount on the z axis
-            playerPiece.position += new Vector3(5f,0f, 0f);
+            playerPiece.position += new Vector3(.5f,0f, 0f);
         }
-         if (Input.GetKeyDown(KeyCode.RightArrow)) 
+        }
+     if (Input.GetKeyDown(KeyCode.RightArrow)) 
         {
-            
+            if(obstacle.position != playerPiece.position +new Vector3(-2.5f,0f, 0f)){
             //modify the playerPiece position by tileAmount on the z axis
-            playerPiece.position += new Vector3(-5f,0f, 0f);
+            playerPiece.position += new Vector3(-.5f,0f, 0f);
         }
-        if (playerPiece.position == warpW.position){
+        }
+        
+    if (playerPiece.position == warpW.position){
         playerPiece.position = playerStart;
+        
+        playerMsg.text = "Try Again";
         
         }
         }
