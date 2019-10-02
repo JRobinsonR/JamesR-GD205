a56d3c950de8617d1662563ed14335ccc9c13148
Steven <stevenadrian02@hotmail.com>
2019-10-02 16:44:08 -0400

    Modified DW and added Space Soccer

---
+++ GD205-28dc38dee9fb368c11a338dff964dc496ecc9dd3/James' Unity Projects/Assets/Scripts/PhysicalMotion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicalMotion : MonoBehaviour
+{
+    public Rigidbody Anvil;
+    public float oForce = 100f;
+
+
+    private void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
