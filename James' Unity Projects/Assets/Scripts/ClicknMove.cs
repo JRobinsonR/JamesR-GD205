4990cbff7ca960d99c361f7c00bb9d3a1688d64d
Steven <stevenadrian02@hotmail.com>
2019-11-20 16:28:46 -0500

    Tried to make a NavMesh

---
+++ James' Unity Projects/Assets/Scripts/ClicknMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserRay : MonoBehaviour
+{
+    public float impactAmount;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray laser = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit = new RaycastHit();
+        Debug.DrawRay(laser.origin, laser.direction, Color.red);
+        if (Physics.Raycast(laser, out hit, 10000f))
+        {
+
+            Debug.Log("Impact" + hit.transform.gameObject.name + "detected");
+            if (hit.transform && Input.GetMouseButton(0))
+            {
+                Vector3 whichDirection = Random.insideUnitSphere;
+                //hit.rigidbody.AddForce(Vector3.up * impactAmount);
+                Destroy(hit.transform.gameObject);
+            }
+        }
+    }
+}
